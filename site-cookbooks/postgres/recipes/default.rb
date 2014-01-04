441e28941e359048b47600741c9b3365908f64da
m <m@gmail.com>
2014-01-05 02:05:14 +0900

    add

--- site-cookbooks/postgres/recipes/default.rb
+++ site-cookbooks/postgres/recipes/default.rb
@@ -10,3 +10,32 @@
 package "postgresql-server" do
      action :install
 end
+
+# DBの初期化コマンドの実行
+# 冪等性を維持するため実行済みか否かをディレクトリの有無で判断
+dataDir = "/var/lib/pgsql/data/"
+if not File.exists? dataDir then
+   execute "postgresql-init" do
+      command "service postgresql initdb"
+   end
+end
+
+# 外部からの接続許可のための設定ファイル書き換え
+template "/var/lib/pgsql/data/pg_hba.conf" do
+   mode 0600
+end
+
+template "/var/lib/pgsql/data/postgresql.conf" do
+   mode 0600
+end
+
+# 再起動と自動起動設定
+service "postgresql" do
+    action [:enable, :restart]
+end
+
+# IPテーブルの設定が邪魔なので停止
+service "iptables" do
+   action [:disable, :stop]
+end
+
